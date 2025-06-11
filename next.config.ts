208a205c2828de8d6acc46ab0a676c5413104e81
haywhy2019 <olusegun.sola19@gmail.com>
2025-06-11 16:40:29 +0100

    added integration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
+import { withPayload } from "@payloadcms/next/withPayload";
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Your Next.js config here
+  experimental: {
+    reactCompiler: false,
+  },
 };
 
-export default nextConfig;
+
+export default withPayload(nextConfig);
