fba0fb02a1474cacddf2e9e3068c6bf2d2c094fd
haywhy2019 <olusegun.sola19@gmail.com>
2025-06-26 07:37:31 +0100

    seach: search filters

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -3,14 +3,5 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col gap-y-4">
-      <div>
-        <Button variant={"elevated"}>I am a button</Button>
-      </div>
-      <div>
-        <Input placeholder="i am an input" />
-      </div>
-    </div>
-  );
+  return <div className="flex flex-col gap-y-4">Home page</div>;
 }
